cc2c2003d6cc0057f5c5ad140a8c655b4ee57040
Wendy Wang <wenwendy.wang@gmail.com>
2018-05-19 17:33:30 +1000

    setup solutions for Monolith, Background Check Service, and Background Check Provider

---
+++ BobBackgroundCheckProvider/BobBackgroundCheckProvider.Processor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BobBackgroundCheckProvider.Processor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Monolith/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Monolith
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
