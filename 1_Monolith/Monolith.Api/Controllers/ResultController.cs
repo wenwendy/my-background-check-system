627810059cb5e446ea0d90b2963e0e94e783bb47
Wendy Wang <wenwendy.wang@gmail.com>
2018-05-22 21:43:08 +1000

    - setup Monolith Api - move invitation creator to own folder

---
+++ 1_Monolith/Monolith.Api/Controllers/ResultController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Monolith.Api.DTOs;
+using Newtonsoft.Json;
+
+namespace Monolith.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ResultController : Controller
+    {
+        private List<ResultDto> _results = new List<ResultDto>();
+
+        [HttpGet]
+        public IEnumerable<ResultDto> Get()
+        {
+            return _results;
+        }
+
+        
+        [HttpPost]
+        public void Post([FromBody] ResultDto result)
+        {
+            Console.WriteLine($"1_Monolith received result: {JsonConvert.SerializeObject(result)}");
+            _results.Add(result);
+        }
+
+    }
+}
---
+++ 1_Monolith/Monolith.BackgroundCheck/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Monolith.BackgroundCheck
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var invitation = GetInvitation();
+
+            Console.WriteLine("posting an invitation ...");
+            var receipt = SendInvitation(invitation);
+
+            Console.WriteLine(receipt);
+        }
+
+
+        private static object GetInvitation()
+        {
+            return new
+            {
+                id = 123,
+                provider = "bob"
+            };
+        }
+
+        private static string SendInvitation(object invitation)
+        {
+            const string uri = "http://localhost:4777/api/invitation";
+
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(uri)
+            };
+
+            var response = client.PostAsJsonAsync("", invitation).Result;
+            var responseString = response.Content.ReadAsStringAsync().Result;
+
+            return responseString;
+        }
+    }
+}
