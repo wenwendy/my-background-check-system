7ad221113f03c93fc32af777e6381d7ea7f90685
Wendy Wang <wenwendy.wang@gmail.com>
2018-05-23 21:23:48 +1000

    run Service processor in parallel

--- 2_Service/MyBackgroundCheckService.Processor/Program.cs
+++ 2_Service/MyBackgroundCheckService.Processor/Program.cs
@@ -1,18 +1,28 @@
-﻿using System.Threading.Tasks;
+﻿using System.Threading;
+using System.Threading.Tasks;
 
 namespace MyBackgroundCheckService.Processor
 {
-    class Program
+    static class Program
     {
         static async Task Main(string[] args)
+        {   
+            var t1 = ProcessInvitation();
+            var t2 = ProcessResult();
+
+            await Task.WhenAll(t1, t2);
+        }
+
+        private static async Task ProcessInvitation()
         {
             var invitationProcessor = new InvitationProcessor();
             await invitationProcessor.Process();
-            
-            //another thread
+        }
+        
+        private static async Task ProcessResult()
+        {
             var resultProcessor = new ResultProcessor();
             await resultProcessor.Process();
-
         }
     }
 }
--- 2_Service/MyBackgroundCheckService.Processor/Transformers/BobInvitationTransformer.cs
+++ 2_Service/MyBackgroundCheckService.Processor/Transformers/BobInvitationTransformer.cs
@@ -6,7 +6,7 @@ namespace MyBackgroundCheckService.Processor.Transformers
     {
         public object Transform(InvitationDto invitation)
         {
-            return invitation;
+            return new { Id = invitation.Id };
         }
 
     }
